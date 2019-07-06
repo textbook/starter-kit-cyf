bb3673380a7b9a9c347350f6bbe8029f95412188
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2019-07-06 17:53:05 +0100

    Handle MongoDB connection error

--- server/api.js
+++ server/api.js
@@ -4,10 +4,13 @@ import { getClient } from "./db";
 
 const api = new Router();
 
-api.get("/", (_, res) => {
+api.get("/", (_, res, next) => {
   const client = getClient();
 
-  client.connect(() => {
+  client.connect((err) => {
+    if (err) {
+      return next(err);
+    }
     res.json({ message: "Hello, world!" });
     client.close();
   });
--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
+/* eslint-disable no-console */
 import http from "http";
 
 import app from "./app";
 
 const port = parseInt(process.env.PORT || "3000");
+
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  console.error(err);
+  res.sendStatus(500);
+});
 
 const server = http.createServer(app);
 
@@ -11,6 +20,5 @@ server.listen(port);
 server.on("listening", () => {
   const addr = server.address();
   const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
-  // eslint-disable-next-line no-console
   console.log(`Listening on ${bind}`);
 });
