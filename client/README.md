cd6b3c27f0c2030665128b7d8bcd58fe81282d85
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2019-07-06 16:48:00 +0100

    Rearrange server and add READMEs

---
+++ client/README.md
@@ -0,0 +1,9 @@
+Client
+======
+
+This directory contains the React client application and related configuration.
+
+It should be pretty familiar if you've used a `create-react-app` app before. It contains the following directories:
+
+  - `src/`: application source code lives here
+  - `webpack/`: configuration lives here (this is hidden from you by CRA)
--- server/app.js
+++ server/app.js
@@ -3,24 +3,38 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 
-import { httpsOnly } from "./helpers";
+import apiRouter from "./api";
 
+const apiRoot = "/api";
 const app = express();
+const staticDir = path.join(__dirname, "static");
 
+// General configuration
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
 
+// require HTTPS in production
 if (app.get("env") === "production") {
-  // require HTTPS in production
   app.enable("trust proxy");
-  app.use(httpsOnly());
+  app.use((req, res, next) => {
+    if (!req.secure) {
+      return res.redirect(301, `https://${req.headers.host}${req.originalUrl}`);
+    }
+    next();
+  });
 }
 
-app.get("/api", (_, res) => {
-  res.json({ message: "Hello, world!" });
-});
+// Load API router
+app.use(apiRoot, apiRouter);
 
-app.use(express.static(path.join(__dirname, "static")));
+// Serve the bundled client from the server
+app.use(express.static(staticDir));
+app.get("*", (req, res, next) => {
+  if (req.url.startsWith(apiRoot)) {
+    return next();
+  }
+  res.sendFile(path.join(staticDir, "index.html"));
+});
 
 export default app;
