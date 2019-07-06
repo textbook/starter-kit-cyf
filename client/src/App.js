c1bbd59b70f511588c99baca9f5a5acee81e7ac3
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2019-07-06 16:15:29 +0100

    Switch from CSS-in-JS to style loader

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
-import withStyles from "react-jss";
 
 import { getMessage } from "./service";
 import logo from "./logo.svg";
+import "./App.css";
 
 export class App extends Component {
-	static propTypes = {
-		classes: PropTypes.object.isRequired,
-	};
 
 	state = { message: "Loading..." };
 
@@ -17,29 +13,13 @@ export class App extends Component {
 	}
 
 	render() {
-		const { classes } = this.props;
 		const { message } = this.state;
 		return (
-			<main role="main">
-				<div>
-					<img className={classes.logo} data-qa="logo" src={logo} alt="Just the React logo" />
-					<h1 className={classes.message} data-qa="message">{message}</h1>
-				</div>
-			</main>
+			<div>
+				<img className="logo" data-qa="logo" src={logo} alt="Just the React logo" />
+				<p className="message" data-qa="message">{message}</p>
+			</div>
 		);
 	}
 }
-
-const styles = (theme) => ({
-	message: {
-		color: theme.palette.text.primary,
-		"&:hover": {
-			backgroundColor: "yellow",
-		},
-	},
-	logo: {
-		width: 100,
-	},
-});
-
-export default withStyles(styles)(App);
+export default App;
