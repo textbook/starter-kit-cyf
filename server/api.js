39e14e1b3d91e4a369923b2da2c40e643a99b552
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2019-07-06 17:53:05 +0100

    Handle MongoDB connection error

--- server/api.js
+++ server/api.js
@@ -4,13 +4,16 @@ import { getClient } from "./db";
 
 const api = new Router();
 
-api.get("/", (_, res) => {
-  const client = getClient();
+api.get("/", (_, res, next) => {
+	const client = getClient();
 
-  client.connect(() => {
-    res.json({ message: "Hello, world!" });
-    client.close();
-  });
+	client.connect((err) => {
+		if (err) {
+			return next(err);
+		}
+		res.json({ message: "Hello, world!" });
+		client.close();
+	});
 });
 
 export default api;
