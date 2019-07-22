9aeced1f8613b5c96de09eb0d2a7a52076c83795
ahmet <ahmet.kio@hotmail.com>
2019-07-22 18:31:21 +0100

    Floating Page Module seşlection, error message, new sllbus link

--- client/src/Components/FloatingMentor.js
+++ client/src/Components/FloatingMentor.js
@@ -16,6 +16,14 @@ import mockStudentsProfiles from "../mockStudentsProfiles.json";
 
 const selectedFloatingMentorName = "Maria";
 
+const h3Style = {
+  color: "red",
+};
+
+const submitStyle = {
+  fontSize: "20px"
+};
+
 export class FloatingMentor extends Component {
   constructor(props) {
     super(props);
@@ -24,7 +32,9 @@ export class FloatingMentor extends Component {
       selectedStudentProfile: null,
       FloatingMentorComments: null,
       commentSubmitted: null,
-      errormessage: null
+      errormessage: null,
+      moduleSelected: null,
+      mentorComments: " "
     };
   }
 
@@ -39,7 +49,7 @@ export class FloatingMentor extends Component {
   handyComments = e => {
     console.log("handyComments working");
     {
-      this.state.clickedStudent === null
+      this.state.moduleSelected === null
         ? this.handleError()
         : this.setState({
             FloatingMentorComments: this.state.commentSubmitted
@@ -50,19 +60,17 @@ export class FloatingMentor extends Component {
   handleError = () => {
     console.log("handleError working");
     this.setState({
-      errormessage: "Please Choose Student by using left dropdown menu"
+      errormessage: "Please Choose A Modul by using below dropdown menu"
     });
   };
 
-  getCurrentDate = (separator = "") => {
-    let newDate = new Date();
-    let date = newDate.getDate();
-    let month = newDate.getMonth() + 1;
-    let year = newDate.getFullYear();
-
-    return `${year}${separator}${
-      month < 10 ? `0${month}` : `${month}`
-    }${separator}${date}`;
+  handleModuleSelection = selected => {
+    console.log("handle Module working....", selected);
+    this.setState({
+      moduleSelected: selected,
+      mentorComments: null,
+      errormessage: null
+    });
   };
 
   render() {
@@ -82,7 +90,7 @@ export class FloatingMentor extends Component {
           padding={20}
           position="center"
           display="flex"
-          flexWrap = "wrap"
+          flexWrap="wrap"
           alignItems="center"
           borderRadius={3}
           border="default"
@@ -103,104 +111,159 @@ export class FloatingMentor extends Component {
               autocompleteProps={{
                 title: "Students"
               }}
-            />            
-        </Pane>
-        {(selectedStudentProfile.length > 0) &&   (
-          <Pane> 
-        <Pane height="auto" width={500} float="left">
-          <Avatar
-            src={
-              selectedStudentProfile.length > 0
-                ? selectedStudentProfile[0].studentPhoto
-                : null
-            }
-            name={
-              selectedStudentProfile.length > 0
-                ? selectedStudentProfile[0].name
-                : null
-            }
-            size={80}
-          />
-          <Table.Body>
-            <Table.Head>
-              <Table.TextCell flexBasis={200} flexShrink={0} flexGrow={0}>
-                Student Name:
-              </Table.TextCell>
-              <Table.TextCell flexBasis={200} flexShrink={0} flexGrow={0}>
-                {selectedStudentProfile.length > 0 &&
-                  selectedStudentProfile[0].name}
-              </Table.TextCell>
-            </Table.Head>
-            <Table.Body>
-              <Table.Row>
-                <Table.TextCell flexBasis={200} flexShrink={0} flexGrow={0}>
-                  Students Soft Skill:
-                </Table.TextCell>
-                <Table.TextCell>
-                  {selectedStudentProfile.length > 0 &&
-                    selectedStudentProfile[0].softSkills}
-                </Table.TextCell>
-              </Table.Row>
-              <Table.Row>
-                <Table.TextCell flexBasis={200} flexShrink={0} flexGrow={0}>
-                  Students Tech Skill:
-                </Table.TextCell>
-                <Table.TextCell>
-                  {selectedStudentProfile.length > 0 &&
-                    selectedStudentProfile[0].techinalSkills}
-                </Table.TextCell>
-              </Table.Row>
-              <Table.Row>
-                <Table.TextCell flexBasis={200} flexShrink={0} flexGrow={0}>
-                  Syllabus:
-                </Table.TextCell>
-                <Table.TextCell>
-                  {selectedStudentProfile.length > 0 && (
-                    <a
-                      target="_blank"
-                      href="https://tinyurl.com/CYF-LDN5CALENDAR"
+            />
+          </Pane>
+          {selectedStudentProfile.length > 0 && (
+            <Pane>
+              <Pane height="auto" width={500} float="left">
+                <Avatar
+                  src={
+                    selectedStudentProfile.length > 0
+                      ? selectedStudentProfile[0].studentPhoto
+                      : null
+                  }
+                  name={
+                    selectedStudentProfile.length > 0
+                      ? selectedStudentProfile[0].name
+                      : null
+                  }
+                  size={80}
+                />
+                <Table.Body>
+                  <Table.Head>
+                    <Table.TextCell
+                      flexBasis={200}
+                      flexShrink={0}
+                      flexGrow={0}
                     >
-                      CYF London-5 Class Syllabus
-                    </a>
-                  )}
-                </Table.TextCell>
-              </Table.Row>
-            </Table.Body>
-          </Table.Body>
-        </Pane>
-               
-        <Pane height={500} width={900} float="left">
-          <Table.Body>
-            <Table.Head>
-              <Table.TextCell flexBasis={300} flexShrink={0} flexGrow={0}>
-                Current Comment:
-              </Table.TextCell>
-            </Table.Head>
-            <Table.Body>
-              <Table.TextCell flexBasis={300} flexShrink={0} flexGrow={0}>
-                <strong>
-                  {selectedStudentProfile.length > 0 &&
-                    this.state.FloatingMentorComments}
-                </strong>
-              </Table.TextCell>
-            </Table.Body>
-            </Table.Body>
+                      Student Name:
+                    </Table.TextCell>
+                    <Table.TextCell
+                      flexBasis={200}
+                      flexShrink={0}
+                      flexGrow={0}
+                    >
+                      {selectedStudentProfile.length > 0 &&
+                        selectedStudentProfile[0].name}
+                    </Table.TextCell>
+                  </Table.Head>
+                  <Table.Body>
+                    <Table.Row>
+                      <Table.TextCell
+                        flexBasis={200}
+                        flexShrink={0}
+                        flexGrow={0}
+                      >
+                        Students Soft Skill:
+                      </Table.TextCell>
+                      <Table.TextCell>
+                        {selectedStudentProfile.length > 0 &&
+                          selectedStudentProfile[0].softSkills}
+                      </Table.TextCell>
+                    </Table.Row>
+                    <Table.Row>
+                      <Table.TextCell
+                        flexBasis={200}
+                        flexShrink={0}
+                        flexGrow={0}
+                      >
+                        Students Tech Skill:
+                      </Table.TextCell>
+                      <Table.TextCell>
+                        {selectedStudentProfile.length > 0 &&
+                          selectedStudentProfile[0].techinalSkills}
+                      </Table.TextCell>
+                    </Table.Row>
+                    <Table.Row>
+                      <Table.TextCell
+                        flexBasis={200}
+                        flexShrink={0}
+                        flexGrow={0}
+                      >
+                        Timeline:
+                      </Table.TextCell>
+                      <Table.TextCell>
+                        {selectedStudentProfile.length > 0 && (
+                          <a
+                            target="_blank"
+                            href="https://tinyurl.com/CYF-LDN5CALENDAR"
+                          >
+                            CYF London-5 Class Timeline
+                          </a>
+                        )}
+                      </Table.TextCell>
+                    </Table.Row>
+                    <Table.Row>
+                      <Table.TextCell
+                        flexBasis={200}
+                        flexShrink={0}
+                        flexGrow={0}
+                      >
+                        Syllabus:
+                      </Table.TextCell>
+                      <Table.TextCell>
+                        {selectedStudentProfile.length > 0 && (
+                          <a
+                            target="_blank"
+                            href="https://codeyourfuture.github.io/syllabus-master/html-css/week-01/lesson.html"
+                          >
+                            CYF London-5 Class Syllabus
+                          </a>
+                        )}
+                      </Table.TextCell>
+                    </Table.Row>
+                  </Table.Body>
+                </Table.Body>
+              </Pane>
 
+              <Pane height={500} width={900} float="left">
+                <Table.Body>
+                  <Table.Head>
+                    <Table.TextCell
+                      flexBasis={300}
+                      flexShrink={0}
+                      flexGrow={0}
+                    >
+                      Current Comment:
+                    </Table.TextCell>
+                  </Table.Head>
+                  <Table.Body>
+                    <Table.TextCell
+                      height={80}
+                      flexBasis={300}
+                      flexShrink={0}
+                      flexGrow={0}
+                    >
+                      <strong style={submitStyle}>
+                        {selectedStudentProfile.length > 0 &&
+                          this.state.FloatingMentorComments}
+                      </strong>
+                      {this.state.FloatingMentorComments && (
+                        <div>
+                          <br /> by {selectedFloatingMentorName} <br />
+                        </div>
+                      )}
+                    </Table.TextCell>
+                  </Table.Body>
+                </Table.Body>
 
-            <Pane
-          width="auto"
-          height="auto"
-          marginLeft={40}
-          padding={10}
-          display="flex"
-          flexDirection= "column"
-          justifyContent = "space-between"
-          borderRadius={3}
-          background="blueTint"
-        >
+                <Pane
+                  width="auto"
+                  height="auto"
+                  marginLeft={40}
+                  padding={10}
+                  display="flex"
+                  flexDirection="column"
+                  justifyContent="space-between"
+                  borderRadius={3}
+                  background="blueTint"
+                >
+                  <Heading>
+                    Previous Comments writen by Floating Mentor (
+                    <strong>{selectedFloatingMentorName}</strong>
+                    ):
+                  </Heading>
 
-<Heading is="h2">Previous Comments writen by Floating Mentor:</Heading>
-                
                   {selectedStudentProfile.length > 0
                     ? selectedStudentProfile[0].floatingMentorcomments.map(
                         s => {
@@ -209,60 +272,81 @@ export class FloatingMentor extends Component {
                             selectedFloatingMentorName
                           )
                             return (
-                              <Pane 
-                              elevation={3}                            
-                              marginLeft={36}
-                              marginBottom = {10}
-                              height="auto" 
-                              padding={10}
-                               width= {400}
-                               background="tint2" borderRadius={3}>
-                                  <Paragraph  marginTop="default">  
+                              <Pane
+                                elevation={3}
+                                marginLeft={36}
+                                marginBottom={10}
+                                height="auto"
+                                padding={10}
+                                width={400}
+                                background="tint2"
+                                borderRadius={3}
+                              >
+                                <Paragraph marginTop="default">
                                   {s.comment}
-                                  
-                                  </Paragraph>
-                            <Text padding={100}> <Strong>Feedback by </Strong>
-                            {s.floatingMentorName} on {s.date} </Text>
-                            </Pane>
-                            
-                                 
+                                </Paragraph>
+                                <Text padding={100}>
+                                  <Strong>Feedback by </Strong>
+                                  {s.floatingMentorName} on {s.date} <br />
+                                  <Text padding={100}>
+                                    {" "}
+                                    <strong> Module: </strong> {s.module}{" "}
+                                  </Text>
+                                </Text>
+                              </Pane>
                             );
-                            
                           else return "";
                         }
                       )
                     : null}
-        </Pane>
-</Pane>
-<Pane>
-              <TextInput
-                width="100%"
-                height={48}
-                alignItems="right"
-                width={800}
-                placeholder="Floating Mentor's Comments..."
-                onChange={e =>
-                  this.setState({ commentSubmitted: e.target.value })
-                }
-              />
-              <Button
-                height={48}
-                appearance="primary"
-                marginTop={20}
-                onClick={this.handyComments}
-              >
-                Submit
-              </Button>
-              <h3> {this.state.errormessage}</h3>
+                </Pane>
+              </Pane>
+              <Pane>
+                <TextInput
+                  width="100%"
+                  height={48}
+                  alignItems="right"
+                  width={800}
+                  placeholder="Floating Mentor's Comments..."
+                  onChange={e =>
+                    this.setState({
+                      commentSubmitted: e.target.value
+                    })
+                  }
+                />
+                <Button
+                  height={48}
+                  appearance="primary"
+                  marginTop={20}
+                  onClick={this.handyComments}
+                >
+                  Submit
+                </Button>
+                <Combobox
+                  items={[
+                    "HTML - CSS",
+                    "JS-Core",
+                    "JS-Core2",
+                    "JS-Core3",
+                    "React",
+                    "Node DB"
+                  ]}
+                  height={38}
+                  onChange={selected =>
+                    this.handleModuleSelection(selected)
+                  }
+                  placeholder="Select Module"
+                  autocompleteProps={{
+                    title: "Select Module"
+                  }}
+                />
+                <h3 style={h3Style}> {this.state.errormessage}</h3>
+              </Pane>
             </Pane>
-            </Pane> )}
-
-
-
-</Pane>
+          )}
+        </Pane>
       </div>
     );
   }
 }
 export default FloatingMentor;
-
