cf7534ddee5552e24d9d15075e294c42ea14b2ce
markazie <markazie@gmail.com>
2019-07-28 12:34:38 +0100

    change result page

--- server/api.js
+++ server/api.js
@@ -32,6 +32,21 @@ api.get("/quiz/:pin?", (req, res) => {
   })
 })
 
+
+api.get("/result/:pin?", (req, res) => {
+  const client = getClient()
+  client.connect(function() {
+    const db = client.db("heroku_shn7149c")
+    const collection = db.collection("results")
+
+    const { pin } = req.params
+
+    collection.find({ pin }).toArray(function(error, result) {
+      res.send(error || result)
+      client.close()
+    })
+  })
+})
 api.post("/quiz", (req, res) => {
   const client = getClient()
   client.connect(function() {
@@ -71,21 +86,6 @@ api.post("/login", function(req, res) {
   })
 })
 
-
-api.post("/result/:pin?", (req, res) => {
-  const client = getClient()
-  client.connect(function() {
-    const db = client.db("heroku_shn7149c")
-    const collection = db.collection("results")
-    
-    const { pin } = req.params
-    
-    collection.find({ pin }, function(error, result) {
-      res.send(error || result.ops[0])
-      client.close()
-    })
-  })
-})
 
 api.put("/answer", (req, res) => {
   const client = getClient()
