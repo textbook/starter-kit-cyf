b0da940a32b244a63d4f85d77f82c1530a77b00c
markazie <markazie@gmail.com>
2019-07-30 17:40:15 +0100

    Front result (#30)
    * change result page
    
    * amend endpoint
    
    * feature/create quiz
    
    * feature/ add multiple questions-create result
    
    * update
    
    * fix duplicate pins
    
    * fix duplicate pins
    
    * fix duplicate pins
    
    * fix duplicate pins

--- server/api.js
+++ server/api.js
@@ -24,7 +24,11 @@ api.get("/get-pin", (req, res) => {
     const collection = db.collection("quiz")
 
     collection.find().toArray(function(error, result) {
-      const pin = result[result.length - 1].pin + Math.floor(Math.random() * 10)
+
+      result = result.sort((a, b) => a.pin - b.pin)
+      const pin =
+        result[result.length - 1].pin + Math.floor(Math.random() * 10) + 1
+
       res.send(error || { pin })
       client.close()
     })
