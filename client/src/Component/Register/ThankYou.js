6e48002e7d926805c2997c02e518835316c110d1
Kadir <kegilmez@gmail.com>
2019-07-31 17:10:15 +0100

    fix thank you

--- client/src/Component/Register/ThankYou.js
+++ client/src/Component/Register/ThankYou.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "../Student/index.css"
-
+​
 function ThankYou() {
   return (
     <main className="mainAttendance">
-      <h1>Thank You Mentor, </h1>
+      <h1>Thank you for Signing in, </h1>
       <div className="attendance">
         <h2>Welcome to the CodeYourFuture Community</h2>
       </div>
     </main>
   );
 }
-
+​
 export default ThankYou;
